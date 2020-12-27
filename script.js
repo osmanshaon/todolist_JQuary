288b2b3484fc9d89ce34bf40c83fa9fe26478e56
Osman Shaon <67813420+osmanshaon@users.noreply.github.com>
2020-12-26 22:40:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$('input').keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $('ul').append('<li>' + todoText + '<span><i class="bookedList"</i></span>');
+  }
+});
+
+
+$('ul').on('click', "span", function (event) {
+  $(this).parent().fadeOut(500, function () {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+
+$('ul').on('click', 'li', function () {
+  $(this).toggleClass('done');
+});
